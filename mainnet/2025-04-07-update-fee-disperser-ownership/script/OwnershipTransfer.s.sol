0d029968a902525f2b0bf6c656a87a0a2ac15432
Jack Chuma <jack.chuma@coinbase.com>
2025-04-08 13:29:18 -0400

    simplify postcheck

--- mainnet/2025-04-07-update-fee-disperser-ownership/script/OwnershipTransfer.s.sol
+++ mainnet/2025-04-07-update-fee-disperser-ownership/script/OwnershipTransfer.s.sol
@@ -24,29 +24,14 @@ contract OwnershipTransfer is MultisigBuilder {
         TARGET = vm.envAddress("TARGET");
     }
 
-    // Confirm the proxy admin owner is now the alias address of the L1 Proxy Admin Owner
-    function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory) internal view override {
-        Vm.AccountAccess memory targetAccess = _findAccountAccess(accesses, TARGET);
+    // Confirm the aliased L1 proxy admin owner is now the owner of the fee dispurser
+    function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal override {
+        address expectedAdmin = L1_SAFE.applyL1ToL2Alias();
 
-        bool seenTarget = false;
+        vm.prank(expectedAdmin);
+        address admin = Proxy(payable(TARGET)).admin();
 
-        for (uint256 i; i < targetAccess.storageAccesses.length; i++) {
-            Vm.StorageAccess memory storageAccess = targetAccess.storageAccesses[i];
-            if (storageAccess.slot == ADMIN_SLOT && storageAccess.isWrite) {
-                require(!seenTarget, "Seen TARGET in storageAccesses");
-
-                require(
-                    storageAccess.previousValue == _addressToBytes32(OWNER_SAFE), "Previous value is not OWNER_SAFE"
-                );
-                require(
-                    storageAccess.newValue == _addressToBytes32(L1_SAFE.applyL1ToL2Alias()), "New value is not L1_SAFE"
-                );
-
-                seenTarget = true;
-            }
-        }
-
-        require(seenTarget, "Did not see TARGET in accesses");
+        require(admin == expectedAdmin, "Admin is not L1_SAFE");
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
@@ -63,23 +48,5 @@ contract OwnershipTransfer is MultisigBuilder {
 
     function _ownerSafe() internal view override returns (address) {
         return OWNER_SAFE;
-    }
-
-    function _findAccountAccess(Vm.AccountAccess[] memory accesses, address target)
-        private
-        pure
-        returns (Vm.AccountAccess memory)
-    {
-        for (uint256 i; i < accesses.length; i++) {
-            if (accesses[i].account == target) {
-                return accesses[i];
-            }
-        }
-
-        revert("Account access not found");
-    }
-
-    function _addressToBytes32(address addr) private pure returns (bytes32) {
-        return bytes32(uint256(uint160(addr)));
     }
 }
