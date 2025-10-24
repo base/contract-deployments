1c06c684bfc914df3549988f92a42d531cccc2be
Joby Thundil <joby.thundil@coinbase.com>
2025-10-24 16:53:22 -0400

    add blacklist and rename retire

---
+++ setup-templates/template-switch-to-permissioned-game-blacklist/script/SwitchToPermissionedGame.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {Vm} from "forge-std/Vm.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
+import {console} from "forge-std/console.sol";
+import {IAnchorStateRegistry} from "@eth-optimism-bedrock/src/dispute/FaultDisputeGame.sol";
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
+import {IDisputeGame, GameStatus} from "@eth-optimism-bedrock/src/dispute/AnchorStateRegistry.sol";
+import {IDisputeGameFactory} from "@eth-optimism-bedrock/interfaces/dispute/IDisputeGameFactory.sol";
+import {FaultDisputeGame} from "@eth-optimism-bedrock/src/dispute/PermissionedDisputeGame.sol";
+import {GameTypes, GameType} from "@eth-optimism-bedrock/src/dispute/lib/Types.sol";
+import {MultisigScript} from "@base-contracts/script/universal/MultisigScript.sol";
+import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
+
+/// @notice This script updates the FaultDisputeGame and PermissionedDisputeGame implementations in the
+///         DisputeGameFactory contract.
+contract SwitchToPermissionedGame is MultisigScript {
+    using stdJson for string;
+
+    // TODO: Confirm expected version
+    string public constant EXPECTED_VERSION = "1.4.1";
+
+    address public immutable OWNER_SAFE;
+    uint256 public immutable L2_DIVERGENCE_BLOCK_NUMBER;
+
+    SystemConfig internal _SYSTEM_CONFIG = SystemConfig(vm.envAddress("SYSTEM_CONFIG"));
+
+    IAnchorStateRegistry anchorStateRegistry;
+    IDisputeGame[] gamesToBlacklist;
+
+    constructor() {
+        OWNER_SAFE = vm.envAddress("OWNER_SAFE");
+        L2_DIVERGENCE_BLOCK_NUMBER = uint64(vm.envUint("L2_DIVERGENCE_BLOCK_NUMBER"));
+    }
+
+    function setUp() public {
+        IDisputeGameFactory dgfProxy = IDisputeGameFactory(_SYSTEM_CONFIG.disputeGameFactory());
+        FaultDisputeGame currentFdg = FaultDisputeGame(address(dgfProxy.gameImpls(GameTypes.CANNON)));
+        anchorStateRegistry = currentFdg.anchorStateRegistry();
+
+        getGamesToBlacklist(dgfProxy);
+    }
+
+    function getGamesToBlacklist(IDisputeGameFactory dgfProxy) internal {
+        uint256 totalNumGames = dgfProxy.gameCount();
+        console.log("total games to search", totalNumGames);
+
+        for (uint256 i = 0; i < totalNumGames; i = i + 1) {
+            (, , IDisputeGame game) = dgfProxy.gameAtIndex(i);
+            if (game.status() == GameStatus.IN_PROGRESS && game.l2SequenceNumber() >= L2_DIVERGENCE_BLOCK_NUMBER) {
+                // this game is in progress and challenges a block at or after the divergence block
+                gamesToBlacklist.push(game);
+            }
+        }
+
+        console.log("total games to blacklist", gamesToBlacklist.length);
+    }
+
+    // Confirm the CURRENT_RETIREMENT_TIMESTAMP is updated to the block time.
+    function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
+        for (uint256 i = 0; i < gamesToBlacklist.length; i = i + 1) {
+            require(anchorStateRegistry.isGameBlacklisted(gamesToBlacklist[i]), "post-110");
+        }
+    }
+
+
+    function _buildCalls() internal view override returns (IMulticall3.Call3Value[] memory) {
+        IMulticall3.Call3Value[] memory calls = new IMulticall3.Call3Value[](gamesToBlacklist.length);
+
+        for (uint256 i = 0; i < gamesToBlacklist.length; i = i + 1) {
+            calls[i] = IMulticall3.Call3Value({
+                target: address(anchorStateRegistry),
+                allowFailure: false,
+                callData: abi.encodeCall(IAnchorStateRegistry.blacklistDisputeGame, (gamesToBlacklist[i])),
+                value: 0
+            });
+        }
+
+        return calls;
+    }
+
+    function _ownerSafe() internal view override returns (address) {
+        return OWNER_SAFE;
+    }
+}
