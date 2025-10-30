f8f9c3fd4e8339aa391006f1f439c630c2193f0d
Joby Thundil <joby.thundil@coinbase.com>
2025-10-30 16:47:05 -0400

    pr feedback

--- setup-templates/template-switch-to-permissioned-game/script/SwitchToPermissionedGame.sol
+++ setup-templates/template-switch-to-permissioned-game/script/SwitchToPermissionedGame.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.15;
 
 import {Vm} from "forge-std/Vm.sol";
-import {stdJson} from "forge-std/StdJson.sol";
 import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
-import {console} from "forge-std/console.sol";
 import {IAnchorStateRegistry} from "@eth-optimism-bedrock/src/dispute/FaultDisputeGame.sol";
 import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
 import {DisputeGameFactory} from "@eth-optimism-bedrock/src/dispute/DisputeGameFactory.sol";
@@ -16,42 +14,36 @@ import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
 /// @notice This script updates the FaultDisputeGame and PermissionedDisputeGame implementations in the
 ///         DisputeGameFactory contract.
 contract SwitchToPermissionedGame is MultisigScript {
-    using stdJson for string;
-
     // TODO: Confirm expected version
     string public constant EXPECTED_VERSION = "1.4.1";
 
     address public immutable OWNER_SAFE;
-    uint64 public immutable CURRENT_RETIREMENT_TIMESTAMP;
+    uint64 public currentRetirementTimestamp;
 
-    SystemConfig internal _SYSTEM_CONFIG = SystemConfig(vm.envAddress("SYSTEM_CONFIG"));
+    SystemConfig internal immutable _SYSTEM_CONFIG = SystemConfig(vm.envAddress("SYSTEM_CONFIG"));
 
     IAnchorStateRegistry anchorStateRegistry;
 
     constructor() {
         OWNER_SAFE = vm.envAddress("OWNER_SAFE");
-        CURRENT_RETIREMENT_TIMESTAMP = uint64(vm.envUint("CURRENT_RETIREMENT_TIMESTAMP"));
     }
 
     function setUp() public {
         DisputeGameFactory dgfProxy = DisputeGameFactory(_SYSTEM_CONFIG.disputeGameFactory());
         FaultDisputeGame currentFdg = FaultDisputeGame(address(dgfProxy.gameImpls(GameTypes.CANNON)));
         anchorStateRegistry = currentFdg.anchorStateRegistry();
-
-        _precheckRetirementTimestamp();
+        currentRetirementTimestamp = anchorStateRegistry.retirementTimestamp();
     }
 
-    // Checks that the current state matches the CURRENT_RETIREMENT_TIMESTAMP
-    function _precheckRetirementTimestamp() internal view {
-        require(anchorStateRegistry.retirementTimestamp() == CURRENT_RETIREMENT_TIMESTAMP, "00");
-    }
-
-    // Confirm the CURRENT_RETIREMENT_TIMESTAMP is updated to the block time.
+    // Confirm the currentRetirementTimestamp is updated to the block time and the
+    // respectedGameType is updated to PERMISSIONED_CANNON.
     function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
         require(anchorStateRegistry.retirementTimestamp() == block.timestamp, "post-110");
-        require(GameType.unwrap(anchorStateRegistry.respectedGameType()) == GameType.unwrap(GameTypes.PERMISSIONED_CANNON), "post-111");
+        require(
+            GameType.unwrap(anchorStateRegistry.respectedGameType()) == GameType.unwrap(GameTypes.PERMISSIONED_CANNON),
+            "post-111"
+        );
     }
-
 
     function _buildCalls() internal view override returns (IMulticall3.Call3Value[] memory) {
         IMulticall3.Call3Value[] memory calls = new IMulticall3.Call3Value[](2);
