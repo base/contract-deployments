58a1ca7fd791c15f6552895c5507b31952f2b5a8
Joby Thundil <joby.thundil@coinbase.com>
2025-11-17 21:57:32 -0500

    use standard validator

--- sepolia/2025-11-07-upgrade-fault-proofs/script/UpgradeWithOpSmartContractManager.sol
+++ sepolia/2025-11-07-upgrade-fault-proofs/script/UpgradeWithOpSmartContractManager.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import {Claim, GameTypes} from "@eth-optimism-bedrock/src/dispute/lib/Types.sol";
+import {Claim} from "@eth-optimism-bedrock/src/dispute/lib/Types.sol";
 import {
     IOPContractsManager,
+    IOPContractsManagerStandardValidator,
     ISystemConfig,
-    IProxyAdmin,
-    IDisputeGameFactory,
-    IFaultDisputeGame
+    IProxyAdmin
 } from "@eth-optimism-bedrock/interfaces/L1/IOPContractsManager.sol";
 import {MultisigScript} from "@base-contracts/script/universal/MultisigScript.sol";
 import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
@@ -22,8 +21,8 @@ contract UpgradeWithOpSmartContractManager is MultisigScript {
     IProxyAdmin public immutable PROXY_ADMIN;
     Claim immutable CANNON_ABSOLUTE_PRESTATE;
 
-    address oldDelayedWethPermissioned;
-    address oldDelayedWethPermissionless;
+    address internal immutable L1_PROXY_ADMIN_OWNER;
+    address internal immutable CHALLENGER;
 
     constructor() {
         OWNER_SAFE = vm.envAddress("OWNER_SAFE");
@@ -31,60 +30,20 @@ contract UpgradeWithOpSmartContractManager is MultisigScript {
         _SYSTEM_CONFIG = ISystemConfig(vm.envAddress("SYSTEM_CONFIG"));
         OP_CONTRACT_MANAGER = IOPContractsManager(vm.envAddress("OP_CONTRACT_MANAGER"));
         CANNON_ABSOLUTE_PRESTATE = Claim.wrap(vm.envBytes32("ABSOLUTE_PRESTATE"));
-
-        IDisputeGameFactory dfg = IDisputeGameFactory(_SYSTEM_CONFIG.disputeGameFactory());
-        IFaultDisputeGame fdg = IFaultDisputeGame(address(dfg.gameImpls(GameTypes.CANNON)));
-        IFaultDisputeGame pfdg = IFaultDisputeGame(address(dfg.gameImpls(GameTypes.PERMISSIONED_CANNON)));
-        oldDelayedWethPermissioned = address(pfdg.weth());
-        oldDelayedWethPermissionless = address(fdg.weth());
+        L1_PROXY_ADMIN_OWNER = vm.envAddress("L1_PROXY_ADMIN_OWNER");
+        CHALLENGER = vm.envAddress("CHALLENGER");
     }
 
     function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
-        IOPContractsManager.Implementations memory impls = OP_CONTRACT_MANAGER.implementations();
+        IOPContractsManagerStandardValidator.ValidationInput memory input =
+            IOPContractsManagerStandardValidator.ValidationInput(
+                PROXY_ADMIN, _SYSTEM_CONFIG, Claim.unwrap(CANNON_ABSOLUTE_PRESTATE), _SYSTEM_CONFIG.l2ChainId()
+            );
 
-        // verify proxyadmin address updates
-        require(PROXY_ADMIN.getProxyImplementation(address(_SYSTEM_CONFIG)) == impls.systemConfigImpl, "00");
-        require(
-            PROXY_ADMIN.getProxyImplementation(address(_SYSTEM_CONFIG.optimismPortal())) == impls.optimismPortalImpl,
-            "01"
-        );
-        require(
-            PROXY_ADMIN.getProxyImplementation(address(_SYSTEM_CONFIG.optimismMintableERC20Factory()))
-                == impls.optimismMintableERC20FactoryImpl,
-            "02"
-        );
-        require(
-            PROXY_ADMIN.getProxyImplementation(address(_SYSTEM_CONFIG.disputeGameFactory()))
-                == impls.disputeGameFactoryImpl,
-            "03"
-        );
+        IOPContractsManagerStandardValidator.ValidationOverrides memory overrides =
+            IOPContractsManagerStandardValidator.ValidationOverrides(L1_PROXY_ADMIN_OWNER, CHALLENGER);
 
-        ISystemConfig.Addresses memory opChainAddrs = _SYSTEM_CONFIG.getAddresses();
-        require(
-            PROXY_ADMIN.getProxyImplementation(opChainAddrs.l1CrossDomainMessenger) == impls.l1CrossDomainMessengerImpl,
-            "04"
-        );
-        require(PROXY_ADMIN.getProxyImplementation(opChainAddrs.l1StandardBridge) == impls.l1StandardBridgeImpl, "05");
-        require(PROXY_ADMIN.getProxyImplementation(opChainAddrs.l1ERC721Bridge) == impls.l1ERC721BridgeImpl, "06");
-
-        IDisputeGameFactory dfg = IDisputeGameFactory(_SYSTEM_CONFIG.disputeGameFactory());
-        IFaultDisputeGame fdg = IFaultDisputeGame(address(dfg.gameImpls(GameTypes.CANNON)));
-        IFaultDisputeGame pfdg = IFaultDisputeGame(address(dfg.gameImpls(GameTypes.PERMISSIONED_CANNON)));
-        Claim fdgAbsolutePrestate = fdg.absolutePrestate();
-        Claim pfdgAbsolutePrestate = pfdg.absolutePrestate();
-
-        // verify FaultDisputeGame and PermissionedDisputeGame absolute prestate
-        require(Claim.unwrap(fdgAbsolutePrestate) == Claim.unwrap(CANNON_ABSOLUTE_PRESTATE), "07");
-        require(Claim.unwrap(pfdgAbsolutePrestate) == Claim.unwrap(CANNON_ABSOLUTE_PRESTATE), "08");
-
-        // verify FaultDisputeGame and PermissionedDisputeGame vm
-        require(address(fdg.vm()) == impls.mipsImpl, "09");
-        require(address(pfdg.vm()) == impls.mipsImpl, "10");
-
-        // verify DelayedWETH doesn't change
-        // https://github.com/ethereum-optimism/optimism/blob/d09c836f818c73ae139f60b717654c4e53712743/packages/contracts-bedrock/src/L1/OPContractsManager.sol#L877
-        require(address(fdg.weth()) == oldDelayedWethPermissionless, "11");
-        require(address(pfdg.weth()) == oldDelayedWethPermissioned, "12");
+        OP_CONTRACT_MANAGER.validateWithOverrides(input, false, overrides);
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3Value[] memory) {
