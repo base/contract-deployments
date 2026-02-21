e3674d2197129c12c31d3d946ed33e79c3b81011
Jack Chuma <jack.chuma@coinbase.com>
2026-02-20 21:43:18 -0500

    update template

--- setup-templates/template-pause-superchain-config/script/PauseSuperchainConfig.s.sol
+++ setup-templates/template-pause-superchain-config/script/PauseSuperchainConfig.s.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.15;
 
 import {Vm} from "forge-std/Vm.sol";
-import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
 import {MultisigScript} from "@base-contracts/script/universal/MultisigScript.sol";
+import {Enum} from "@base-contracts/script/universal/IGnosisSafe.sol";
 
 interface ISystemConfig {
     function superchainConfig() external view returns (address);
@@ -12,22 +12,22 @@ interface ISystemConfig {
 
 interface ISuperchainConfig {
     function pause(address _identifier) external;
-    function paused() external view returns (bool);
+    function paused(address) external view returns (bool);
 }
 
 contract PauseSuperchainConfig is MultisigScript {
     address public immutable INCIDENT_SAFE = vm.envAddress("INCIDENT_SAFE");
     address public immutable SYSTEM_CONFIG = vm.envAddress("SYSTEM_CONFIG");
 
-    function _buildCalls() internal view override returns (IMulticall3.Call3Value[] memory) {
-        IMulticall3.Call3Value[] memory calls = new IMulticall3.Call3Value[](1);
+    function _buildCalls() internal view override returns (Call[] memory) {
+        Call[] memory calls = new Call[](1);
 
         address superchainConfig = ISystemConfig(SYSTEM_CONFIG).superchainConfig();
 
-        calls[0] = IMulticall3.Call3Value({
+        calls[0] = Call({
+            operation: Enum.Operation.Call,
             target: superchainConfig,
-            allowFailure: false,
-            callData: abi.encodeCall(ISuperchainConfig.pause, (address(0))),
+            data: abi.encodeCall(ISuperchainConfig.pause, (address(0))),
             value: 0
         });
 
@@ -36,7 +36,7 @@ contract PauseSuperchainConfig is MultisigScript {
 
     function _postCheck(Vm.AccountAccess[] memory, Simulation.Payload memory) internal view override {
         address superchainConfig = ISystemConfig(SYSTEM_CONFIG).superchainConfig();
-        bool paused = ISuperchainConfig(superchainConfig).paused();
+        bool paused = ISuperchainConfig(superchainConfig).paused(address(0));
         require(paused == true, "PauseSuperchainConfig: chain is not paused");
     }
 
