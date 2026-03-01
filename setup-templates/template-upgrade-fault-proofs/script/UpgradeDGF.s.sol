86dae61a2cf3dcff180fe466c2a99f3df0446afd
Jack Chuma <jack.chuma@coinbase.com>
2026-03-01 08:05:32 -0500

    fix upgrade fault proofs template build and formatting

--- setup-templates/template-upgrade-fault-proofs/script/UpgradeDGF.s.sol
+++ setup-templates/template-upgrade-fault-proofs/script/UpgradeDGF.s.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.24;
 
 import {Vm} from "forge-std/Vm.sol";
 import {stdJson} from "forge-std/StdJson.sol";
-import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {console} from "forge-std/console.sol";
 
 import {LibString} from "@solady/utils/LibString.sol";
 
 import {MultisigScript} from "@base-contracts/script/universal/MultisigScript.sol";
 import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
+import {Enum} from "@base-contracts/script/universal/IGnosisSafe.sol";
 
 interface IDisputeGameFactory {
     function gameImpls(uint32) external view returns (address);
@@ -125,19 +125,19 @@ contract UpgradeDGF is MultisigScript {
         require(rootBlockNumber != 0, "check-310");
     }
 
-    function _buildCalls() internal view override returns (IMulticall3.Call3Value[] memory) {
-        IMulticall3.Call3Value[] memory calls = new IMulticall3.Call3Value[](2);
+    function _buildCalls() internal view override returns (Call[] memory) {
+        Call[] memory calls = new Call[](2);
 
-        calls[0] = IMulticall3.Call3Value({
+        calls[0] = Call({
+            operation: Enum.Operation.Call,
             target: address(dgfProxy),
-            allowFailure: false,
-            callData: abi.encodeCall(IDisputeGameFactory.setImplementation, (CANNON, fdgImpl)),
+            data: abi.encodeCall(IDisputeGameFactory.setImplementation, (CANNON, fdgImpl)),
             value: 0
         });
-        calls[1] = IMulticall3.Call3Value({
+        calls[1] = Call({
+            operation: Enum.Operation.Call,
             target: address(dgfProxy),
-            allowFailure: false,
-            callData: abi.encodeCall(IDisputeGameFactory.setImplementation, (PERMISSIONED_CANNON, pdgImpl)),
+            data: abi.encodeCall(IDisputeGameFactory.setImplementation, (PERMISSIONED_CANNON, pdgImpl)),
             value: 0
         });
 
