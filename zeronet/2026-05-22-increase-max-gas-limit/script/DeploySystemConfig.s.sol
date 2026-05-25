c397b82a882e8700b6ac20bff98ecbd2842db430
William Law <williamlaw.wtl@gmail.com>
2026-05-25 11:09:34 -0400

    feat: increase max gas limit on zeronet (#697)
    * feat: increase max gas limit on zeronet
    
    * feedback
    
    * desc fixes
    
    * records: add execution artifacts and mark task EXECUTED
    
    * revert status to READY TO SIGN for signer UI
    
    * records: add SC approval execution artifacts
    
    * mark task EXECUTED
    
    * records: add final execution artifact and mark EXECUTED
    
    * bookkeeping
    
    * add artifacts to readme

---
+++ zeronet/2026-05-22-increase-max-gas-limit/script/DeploySystemConfig.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {SystemConfig} from "@base-contracts/src/L1/SystemConfig.sol";
+
+contract DeploySystemConfigScript is Script {
+    SystemConfig systemConfigImpl;
+
+    function run() public {
+        vm.startBroadcast();
+        systemConfigImpl = new SystemConfig();
+        console.log("SystemConfig implementation deployed at: ", address(systemConfigImpl));
+        vm.stopBroadcast();
+
+        string memory obj = "root";
+        string memory json = vm.serializeAddress(obj, "systemConfig", address(systemConfigImpl));
+        vm.writeJson(json, "addresses.json");
+
+        _postCheck();
+    }
+
+    function _postCheck() internal view {
+        require(
+            keccak256(bytes(SystemConfig(systemConfigImpl).version()))
+                == keccak256("3.13.2+max-gas-limit-2000M"),
+            "SystemConfig version mismatch"
+        );
+
+        require(SystemConfig(systemConfigImpl).maximumGasLimit() == 2_000_000_000, "Maximum gas limit mismatch");
+    }
+}
