380c6b14ec6d1ad026c96f6b2ba789ba4773ad2c
Thanh Trinh <thanh.trinh@coinbase.com>
2026-08-27 13:02:20 -0500

    Revert "fix(CHAIN-4644): use network-scoped address config"
    This reverts commit 486c5f734e09a3b2853ee63ddcb7b66ba8baa9b6.

--- active/evm/tasks/2026-08-25-update-sepolia-balance-tracker-profit-wallet/script/UpgradeBalanceTracker.s.sol
+++ active/evm/tasks/2026-08-25-update-sepolia-balance-tracker-profit-wallet/script/UpgradeBalanceTracker.s.sol
@@ -12,14 +12,22 @@ contract UpgradeBalanceTracker is Script {
     address payable internal immutable CURRENT_PROFIT_WALLET;
     address payable internal immutable NEW_PROFIT_WALLET;
     address internal immutable NEW_IMPLEMENTATION;
+    address payable internal immutable SYSTEM_ADDRESS_0;
+    address payable internal immutable SYSTEM_ADDRESS_1;
+    uint256 internal immutable TARGET_BALANCE_0;
+    uint256 internal immutable TARGET_BALANCE_1;
 
     constructor() {
+        PROXY_ADMIN = vm.envAddress("PROXY_ADMIN");
         BALANCE_TRACKER = payable(vm.envAddress("BALANCE_TRACKER"));
+        CURRENT_PROFIT_WALLET = payable(vm.envAddress("CURRENT_PROFIT_WALLET"));
+        NEW_PROFIT_WALLET = payable(vm.envAddress("NEW_PROFIT_WALLET"));
+        SYSTEM_ADDRESS_0 = payable(vm.envAddress("SYSTEM_ADDRESS_0"));
+        SYSTEM_ADDRESS_1 = payable(vm.envAddress("SYSTEM_ADDRESS_1"));
+        TARGET_BALANCE_0 = vm.envUint("TARGET_BALANCE_0");
+        TARGET_BALANCE_1 = vm.envUint("TARGET_BALANCE_1");
 
         string memory json = vm.readFile(vm.envString("ADDRESSES_JSON"));
-        PROXY_ADMIN = vm.parseJsonAddress(json, ".proxyAdmin");
-        CURRENT_PROFIT_WALLET = payable(vm.parseJsonAddress(json, ".currentProfitWallet"));
-        NEW_PROFIT_WALLET = payable(vm.parseJsonAddress(json, ".newProfitWallet"));
         NEW_IMPLEMENTATION = vm.parseJsonAddress(json, ".balanceTrackerImplementation");
 
         require(NEW_IMPLEMENTATION.code.length > 0, "implementation has no code");
@@ -29,27 +37,25 @@ contract UpgradeBalanceTracker is Script {
         );
         vm.prank(PROXY_ADMIN);
         require(Proxy(BALANCE_TRACKER).admin() == PROXY_ADMIN, "proxy admin mismatch");
-        require(
-            BalanceTracker(BALANCE_TRACKER).PROFIT_WALLET() == CURRENT_PROFIT_WALLET, "current profit wallet mismatch"
-        );
+        _checkState(CURRENT_PROFIT_WALLET);
     }
 
     function run() external {
-        BalanceTracker balanceTracker = BalanceTracker(BALANCE_TRACKER);
-        address systemAddress0 = balanceTracker.systemAddresses(0);
-        address systemAddress1 = balanceTracker.systemAddresses(1);
-        uint256 targetBalance0 = balanceTracker.targetBalances(0);
-        uint256 targetBalance1 = balanceTracker.targetBalances(1);
-
         vm.broadcast(PROXY_ADMIN);
         Proxy(BALANCE_TRACKER).upgradeTo(NEW_IMPLEMENTATION);
 
         vm.prank(PROXY_ADMIN);
         require(Proxy(BALANCE_TRACKER).implementation() == NEW_IMPLEMENTATION, "implementation mismatch");
-        require(balanceTracker.PROFIT_WALLET() == NEW_PROFIT_WALLET, "new profit wallet mismatch");
-        require(balanceTracker.systemAddresses(0) == systemAddress0, "system address 0 changed");
-        require(balanceTracker.systemAddresses(1) == systemAddress1, "system address 1 changed");
-        require(balanceTracker.targetBalances(0) == targetBalance0, "target balance 0 changed");
-        require(balanceTracker.targetBalances(1) == targetBalance1, "target balance 1 changed");
+        _checkState(NEW_PROFIT_WALLET);
+    }
+
+    function _checkState(address expectedProfitWallet) internal view {
+        BalanceTracker balanceTracker = BalanceTracker(BALANCE_TRACKER);
+
+        require(balanceTracker.PROFIT_WALLET() == expectedProfitWallet, "profit wallet mismatch");
+        require(balanceTracker.systemAddresses(0) == SYSTEM_ADDRESS_0, "system address 0 mismatch");
+        require(balanceTracker.systemAddresses(1) == SYSTEM_ADDRESS_1, "system address 1 mismatch");
+        require(balanceTracker.targetBalances(0) == TARGET_BALANCE_0, "target balance 0 mismatch");
+        require(balanceTracker.targetBalances(1) == TARGET_BALANCE_1, "target balance 1 mismatch");
     }
 }
